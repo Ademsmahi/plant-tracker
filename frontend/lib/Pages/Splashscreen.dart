1b76bd5abdacb8808f57ed3dcc837be537fdd1e8
Ademsmahi <adem.smahi@univ-constantine2.dz>
2025-05-07 00:27:41 +0100

    Update Splashscreen.dart

--- frontend/lib/Pages/Splashscreen.dart
+++ frontend/lib/Pages/Splashscreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -10,22 +9,8 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 2), () async {
-      final prefs = await SharedPreferences.getInstance();
-      final token = prefs.getString('user_token');
-      final role = prefs.getString('user_role');
-      final id = prefs.getString('user_id');
-      if(token != null) {
-        if (role == 'gestionnaire') {
-          Navigator.pushReplacementNamed(context, '/managerHome');
-        } else if (role == 'technicien') {
-          //TODO navigate to technicien
-        } else if (role == "agriculteur"){
-          //TODO navigate to agriculteur
-        }
-      }else {
-        Navigator.pushReplacementNamed(context, '/login');
-      }
+    Future.delayed(Duration(seconds: 4), () {
+      Navigator.pushReplacementNamed(context, '/login');
     });
   }
 
@@ -37,17 +22,8 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/plantTracker.png',  height: MediaQuery.of(context).size.height * 0.25,),
+            Image.asset('assets/plantTracker.png',  height: MediaQuery.of(context).size.height * 0.5,),
             SizedBox(height: 10),
-            Text(
-              'PlantTracker',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.green),
-            ),
-            Text(
-              'Track your greenhouse anytime, anywhere.',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 14, color: Colors.black87),
-            ),
           ],
         ),
       ),
