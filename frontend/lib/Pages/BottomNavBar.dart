308b88dda4b90d3888975d6601dc4845753f88c4
Ademsmahi <adem.smahi@univ-constantine2.dz>
2025-05-07 00:29:22 +0100

    Update BottomNavBar.dart

--- frontend/lib/Pages/BottomNavBar.dart
+++ frontend/lib/Pages/BottomNavBar.dart
@@ -22,10 +22,10 @@ class BottomNavBar extends StatelessWidget {
       showUnselectedLabels: false,
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.camera_alt), label: "Scan"), // NEW
+        BottomNavigationBarItem(icon: Icon(Icons.camera_alt), label: "Scan"),
         BottomNavigationBarItem(icon: Icon(Icons.insights), label: "Stats"),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
         BottomNavigationBarItem(icon: Icon(Icons.article), label: "Report"),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
       ],
     );
   }
